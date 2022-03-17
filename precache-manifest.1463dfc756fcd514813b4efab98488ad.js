59e27dce404f9af0f6e3771fc8e5f2bd527081d8
Maria Z <zaymaria87@gmail.com>
2022-03-17 17:29:32 +0100

    Updates

---
+++ precache-manifest.1463dfc756fcd514813b4efab98488ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8aa3e4279b9646facc27",
+    "url": "/final-project/static/css/main.77b66370.chunk.css"
+  },
+  {
+    "revision": "8aa3e4279b9646facc27",
+    "url": "/final-project/static/js/main.8aa3e427.chunk.js"
+  },
+  {
+    "revision": "6f02a2bd02759dab58cc",
+    "url": "/final-project/static/js/1.6f02a2bd.chunk.js"
+  },
+  {
+    "revision": "dfe220caf4e630ef7987",
+    "url": "/final-project/static/js/runtime~main.dfe220ca.js"
+  },
+  {
+    "revision": "ab265a67ec077a9a72a01d8ca8d29131",
+    "url": "/final-project/index.html"
+  }
+];
