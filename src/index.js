72db90fd055a769992c4703262de3cc7821d5b02
Maria Z <zaymaria87@gmail.com>
2022-03-17 19:21:34 +0100

    gh pages fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AppLayout from './containers/AppLayout';
 import Caught from './routes/Caught';
 import Description from './routes/Description';
@@ -10,20 +10,19 @@ import './index.css';
 ReactDOM.render(
   <Router>
     <AppLayout />
-      <Routes>
-        <Route path="/" element={<PokemonList />} />
-        <Route path="/caught" element={<Caught />} />
-        <Route exact path="/description/:id" element={<Description />} />
-        <Route
-          path="*"
-          element={
-            <main style={{ padding: "1rem", color: 'darkblue', fontWeight: 800, fontSize: 26 }}>
-              <p>There's nothing here!</p>
-            </main>
-          }
-          />
-      </Routes>
-    {/* </AppLayout> */}
+    <Routes>
+      <Route path="/" element={<PokemonList />} />
+      <Route path="/caught" element={<Caught />} />
+      <Route exact path="/description/:id" element={<Description />} />
+      <Route
+        path="*"
+        element={
+          <main style={{ padding: "1rem", color: 'darkblue', fontWeight: 800, fontSize: 26 }}>
+            <p>There's nothing here!</p>
+          </main>
+        }
+        />
+    </Routes>
   </Router>,
   document.getElementById('root')
 );
